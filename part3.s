c2d5566413f3f835a7d4f3c69662be0359da99ee
user6001 <105407600+user6001@users.noreply.github.com>
2022-05-17 17:49:29 -0400

    Add files via upload

---
+++ part3.s
@@ -0,0 +1,62 @@
+	
+.global _start
+.text
+_start:	
+	#open frame buffer 1 
+	mov r0,#1
+	bl openfb
+	cmp r0,#0
+	blt _start_exit
+
+
+    
+    #set the color to blue
+	mov r8,r0
+	mov r0,#31
+	mov r1,#0
+	mov r2,#0	
+	bl getColor
+	mov r6,r0
+    
+    #initializing the for loop variables
+    mov r0,#0
+    mov r1,#0	
+    mov r2, r6
+    mov r10, #0
+    mov r12, #0
+   
+firstLoop:
+    cmp r0, #8
+    beq _start_exit
+    #set pixel to red
+    bl setPixel
+    add r12,#1
+    mov r0,r12
+    add r10,#1
+    mov r1,r10 
+    b firstLoop
+    
+    #initializing the for lop variables
+    mov r0,#0
+    mov r1,#0	
+    mov r2, r6
+    mov r10, #7
+    mov r12, #0
+
+secondLoop:    
+    cmp r1, #8
+    beq _start_exit
+    #set pixel to red
+    bl setPixel
+    add r12,#1
+    mov r0,r12
+    sub r10,#1
+    mov r1,r10 
+    b secondLoop
+   
+
+_start_exit: 
+	#clean exit
+	mov r7,#1
+	svc #0
+
