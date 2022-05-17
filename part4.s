45f5d1b6135f0bba74f9889b0beafa31a65f95fb
user6001 <105407600+user6001@users.noreply.github.com>
2022-05-17 18:53:29 -0400

    Add files via upload

--- part4.s
+++ part4.s
@@ -14,54 +14,43 @@ _start:
     mov r2,#31    
     bl getColor
     mov r6,r0
-
-    #set pixel 5,2 to the blue color stored in r6 
+#the C
+    #set pixel 0,1 to 0,4
     mov r2,r6
-    mov r0,#5
+    mov r0,#0
+    mov r1,#1    
+    bl setPixel
+    mov r2,r6
+    mov r0,#0
     mov r1,#2    
     bl setPixel
-    #set pixel 6,2 to the blue color stored in r6 
     mov r2,r6
-    mov r0,#6
-    mov r1,#2    
+    mov r0,#0
+    mov r1,#3    
     bl setPixel
-    #set pixel 1,1 to the blue color stored in r6 
+    mov r2,r6
+    mov r0,#0
+    mov r1,#4    
+    bl setPixel
+    #set 1,1 2,1 1,4 2,4
     mov r2,r6
     mov r0,#1
     mov r1,#1    
     bl setPixel
-    #set pixel 2,2 to the blue color stored in r6 
     mov r2,r6
     mov r0,#2
-    mov r1,#2    
-    bl setPixel
-    #set pixel 3,1 to the blue color stored in r6 
-    mov r2,r6
-    mov r0,#3
-    mov r1,#1    
-    bl setPixel
-    #below is 0,0 to 0,3
-    mov r2,r6
-    mov r0,#0
-    mov r1,#0    
-    bl setPixel
-    mov r2,r6
-    mov r0,#0
     mov r1,#1    
     bl setPixel
     mov r2,r6
-    mov r0,#0
-    mov r1,#2    
+    mov r0,#1
+    mov r1,#4    
     bl setPixel
     mov r2,r6
-    mov r0,#0
-    mov r1,#3
-    bl setPixel
-#below is 4,0 to 4,3
-    mov r2,r6
-    mov r0,#4
-    mov r1,#0    
-    bl setPixel
+    mov r0,#2
+    mov r1,#4    
+    bl setPixel    
+#the K
+    #set 4,1 to 4,6
     mov r2,r6
     mov r0,#4
     mov r1,#1    
@@ -72,26 +61,47 @@ _start:
     bl setPixel
     mov r2,r6
     mov r0,#4
-    mov r1,#3
+    mov r1,#3    
     bl setPixel
-#below is 7,0 to 7,3
     mov r2,r6
-    mov r0,#7
-    mov r1,#0    
+    mov r0,#4
+    mov r1,#4    
+    bl setPixel
+    mov r2,r6
+    mov r0,#4
+    mov r1,#5    
+    bl setPixel
+    mov r2,r6
+    mov r0,#4
+    mov r1,#6    
+    bl setPixel
+    #set 5,3 5,4
+    mov r2,r6
+    mov r0,#5
+    mov r1,#3    
+    bl setPixel
+    mov r2,r6
+    mov r0,#5
+    mov r1,#4    
+    #set 6,2 7,1 6,5 7,6
+    mov r2,r6
+    mov r0,#6
+    mov r1,#2    
     bl setPixel
     mov r2,r6
     mov r0,#7
     mov r1,#1    
     bl setPixel
+    #set 5,3 5,4
     mov r2,r6
-    mov r0,#7
-    mov r1,#2    
+    mov r0,#6
+    mov r1,#5    
     bl setPixel
     mov r2,r6
     mov r0,#7
-    mov r1,#3
-    bl setPixel
+    mov r1,#6    
 
+    bl closefb
 
 _start_exit: 
     #clean exit
