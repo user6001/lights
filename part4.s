57f3f4719681fd077d176b06315e1b605d9c57f4
user6001 <105407600+user6001@users.noreply.github.com>
2022-05-17 18:45:43 -0400

    Add files via upload

--- part4.s
+++ part4.s
@@ -40,63 +40,102 @@ _start:
 	mov r2,r6
 	mov r0,#3
 	mov r1,#1	
-	bl setPixel
+.global _start
+.text
+_start:    
+    #open frame buffer 1 
+    mov r0,#1
+    bl openfb
+    cmp r0,#0
+    blt _start_exit
 
-    #initializing the for lop variables
-    mov r0,#0
-    mov r1,#0	
-    mov r2, r6
-    mov r10, #0
-    mov r12, #7
+    #get the color purple store in r6
+    mov r8,r0
+    mov r0,#31
+    mov r1,#0
+    mov r2,#31    
+    bl getColor
+    mov r6,r0
 
-firstLoop:
-    cmp r0, #8
-    beq _start_exit
-    #set pixel to red
+    #set pixel 5,2 to the blue color stored in r6 
+    mov r2,r6
+    mov r0,#5
+    mov r1,#2    
     bl setPixel
-    mov r0,r12
-    add r10,#1
-    mov r1,r10 
-    b firstLoop
-
-    #initializing the for lop variables
-    mov r0,#0
-    mov r1,#0	
-    mov r2, r6
-    mov r10, #0
-    mov r12, #0
-
-secondLoop:
-    cmp r0, #8
-    beq _start_exit
-    #set pixel to red
+    #set pixel 6,2 to the blue color stored in r6 
+    mov r2,r6
+    mov r0,#6
+    mov r1,#2    
     bl setPixel
-    mov r0,r12
-    add r10,#1
-    mov r1,r10 
-    b secondLoop
-
-    #initializing the for lop variables
-    mov r0,#0
-    mov r1,#0	
-    mov r2, r6
-    mov r10, #0
-    mov r12, #4
-
-thirdLoop:
-    cmp r0, #8
-    beq _start_exit
-    #set pixel to red
+    #set pixel 1,1 to the blue color stored in r6 
+    mov r2,r6
+    mov r0,#1
+    mov r1,#1    
     bl setPixel
-    mov r0,r12
-    add r10,#1
-    mov r1,r10 
-    b thirdLoop
-
+    #set pixel 2,2 to the blue color stored in r6 
+    mov r2,r6
+    mov r0,#2
+    mov r1,#2    
+    bl setPixel
+    #set pixel 3,1 to the blue color stored in r6 
+    mov r2,r6
+    mov r0,#3
+    mov r1,#1    
+    bl setPixel
+    #below is 0,0 to 0,3
+    mov r2,r6
+    mov r0,#0
+    mov r1,#0    
+    bl setPixel
+    mov r2,r6
+    mov r0,#0
+    mov r1,#1    
+    bl setPixel
+    mov r2,r6
+    mov r0,#0
+    mov r1,#2    
+    bl setPixel
+    mov r2,r6
+    mov r0,#0
+    mov r1,#3
+    bl setPixel
+#below is 4,0 to 4,3
+    mov r2,r6
+    mov r0,#4
+    mov r1,#0    
+    bl setPixel
+    mov r2,r6
+    mov r0,#4
+    mov r1,#1    
+    bl setPixel
+    mov r2,r6
+    mov r0,#4
+    mov r1,#2    
+    bl setPixel
+    mov r2,r6
+    mov r0,#4
+    mov r1,#3
+    bl setPixel
+#below is 7,0 to 7,3
+    mov r2,r6
+    mov r0,#7
+    mov r1,#0    
+    bl setPixel
+    mov r2,r6
+    mov r0,#7
+    mov r1,#1    
+    bl setPixel
+    mov r2,r6
+    mov r0,#7
+    mov r1,#2    
+    bl setPixel
+    mov r2,r6
+    mov r0,#7
+    mov r1,#3
+    bl setPixel
 
 
 _start_exit: 
-	#clean exit
-	mov r7,#1
-	svc #0
-
+    #clean exit
+    mov r7,#1
+    svc #0
