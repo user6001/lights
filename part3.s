1bd8b9f21cbaee0bb267f67effadc0e3201d0999
user6001 <105407600+user6001@users.noreply.github.com>
2022-05-17 18:06:32 -0400

    Add files via upload

--- part3.s
+++ part3.s
@@ -1,33 +1,33 @@
 	
 .global _start
 .text
-_start:	
-	#open frame buffer 1 
-	mov r0,#1
-	bl openfb
-	cmp r0,#0
-	blt _start_exit
+_start:    
+    #open frame buffer 1 
+    mov r0,#1
+    bl openfb
+    cmp r0,#0
+    blt _start_exit
 
 
     
     #set the color to blue
-	mov r8,r0
-	mov r0,#31
-	mov r1,#0
-	mov r2,#0	
-	bl getColor
-	mov r6,r0
+    mov r8,r0
+    mov r0,#31
+    mov r1,#0
+    mov r2,#0    
+    bl getColor
+    mov r6,r0
     
     #initializing the for loop variables
     mov r0,#0
-    mov r1,#0	
+    mov r1,#0    
     mov r2, r6
     mov r10, #0
     mov r12, #0
-   
+    b firstLoop
 firstLoop:
     cmp r0, #8
-    beq _start_exit
+    b thirdLoop
     #set pixel to red
     bl setPixel
     add r12,#1
@@ -37,26 +37,26 @@ firstLoop:
     b firstLoop
     
     #initializing the for lop variables
-    mov r0,#0
-    mov r1,#0	
+thirdLoop:
+    mov r0,#7
+    mov r1,#0    
     mov r2, r6
-    mov r10, #7
-    mov r12, #0
-
+    mov r10, #0
+    mov r12, #7
+    b secondLoop
 secondLoop:    
     cmp r1, #8
     beq _start_exit
     #set pixel to red
     bl setPixel
-    add r12,#1
+    sub r12,#1
     mov r0,r12
-    sub r10,#1
+    add r10,#1
     mov r1,r10 
     b secondLoop
    
 
 _start_exit: 
-	#clean exit
-	mov r7,#1
-	svc #0
-
+    #clean exit
+    mov r7,#1
+    svc #0
