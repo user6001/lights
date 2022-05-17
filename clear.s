4cfc35b70b028eddcdb81ecb144c13779dfabcde
user6001 <105407600+user6001@users.noreply.github.com>
2022-05-17 15:42:32 -0400

    Add files via upload

--- clear.s
+++ clear.s
@@ -20,24 +20,31 @@ _start:
     
     #initializing the for loop variables
     mov r0,#0
-	mov r1,#0	
+    mov r1,#0	
     mov r2, r6
-
+    b secondLoop
     #for loop to clear the entire thing 
 firstLoop:
     cmp r0, #8
-    beq done
+    bl closefb
+    cmp r0, #8
+    beq _start_exit
+    add r0,#1
+    mov r1,#0
+    b secondLoop
+    ####added lines here it comes up at 0,8
+    
 secondLoop:    
     cmp r1, #8
-    beq done
+    beq firstLoop
     #setPixel to clear
     bl setPixel
     add r1,r1,#1 
     b secondLoop
-    add r0,r0,#1 
-    b firstLoop
+   ## add r0,r0,#1 
+   ## b firstLoop
 	#cleanup the data structures created by openfb and close the framebuffer device
-	bl closefb
+	##bl closefb
 
 _start_exit: 
 	#clean exit
